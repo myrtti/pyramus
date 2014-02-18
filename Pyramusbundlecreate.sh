b95ce66441925c94d13168f3842993eb7a61e8fe
Miia Ranta <myrtti@users.noreply.github.com>
2014-02-18 09:23:48 +0200

    update before possible merge...

--- Pyramusbundlecreate.sh
+++ Pyramusbundlecreate.sh
@@ -43,18 +43,13 @@ mkdir /opt/jboss && chown jboss:jboss /opt/jboss
 sudo -u jboss tar -xvf /tmp/pyramusbundle/jboss-as-7.1.1.Final.tar.gz -C /opt/jboss/
 sudo -u jboss unzip jboss-as-mysql-module-7.1.1.zip -d /opt/jboss/jboss-as-7.1.1.Final/modules/
 
-sudo -u jboss unzip /tmp/pyramusbundle/xmldb-updater-cmd-2.0.1.zip /opt/jboss/
+sudo -u jboss unzip /tmp/pyramusbundle/xmldb-updater-cmd-2.0.1.zip -d /opt/jboss/
 sudo -u jboss mkdir /opt/jboss/pyramus-updates
 sudo -u jboss unzip /tmp/pyramusbundle/updates-0.6.3.zip -d /opt/jboss/pyramus-updates
 
 # Deploying Pyramus components
 sudo -u jboss cp pyramus-0.6.4.war /opt/jboss/jboss-as-7.1.1.Final/standalone/deployments/ROOT.war
 sudo -u jboss cp reports-0.6.4.war /opt/jboss/jboss-as-7.1.1.Final/standalone/deployments/PyramusReports.war
-
-# Final change: configurations
-echo "patching configurations"
-cd /opt/jboss
-sudo -u jboss patch --verbose -p1 -i /home/pyro/pyramus-standalone-patch.diff
 
 echo "creating package to /tmp/Pyramusbundle-0.6.4-`date -I`.tgz"
 
